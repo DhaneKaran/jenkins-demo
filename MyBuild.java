dd35648812f35281d3ec3019721316983d6f066e
DhaneKaran <karandhane0808@gmail.com>
2024-08-21 10:49:37 +0530

    Add files via upload

---
+++ MyBuild.java
@@ -0,0 +1,9 @@
+//import java.lang.*;
+class MyBuild
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hello");
+		System.out.println("Hello!\nDhane Karan from TEIT here...!");
+	}
+}
